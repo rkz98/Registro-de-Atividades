af8bc92c60ca3fa4534c89b9fa8c4e7c3db2dffa
rkz98 <kauerodrigues98@gmail.com>
2019-05-01 17:26:06 -0300

    insert buttons and functions to add tasks.

--- app/src/main/java/com/rkz98/registrodeatividades/MainActivity.java
+++ app/src/main/java/com/rkz98/registrodeatividades/MainActivity.java
@@ -1,11 +1,16 @@
 package com.rkz98.registrodeatividades;
 
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.rkz98.registrodeatividades.Adapters.TasksAdapter;
@@ -15,22 +20,36 @@ import com.rkz98.registrodeatividades.Models.Enum.Days;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
-    private List<DayOfWeek> daysOfWeek;
+    private List<DayOfWeek> daysOfWeek = new ArrayList<>();
+    private final TasksAdapter tasksAdapter  = new TasksAdapter(this, daysOfWeek);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.daysOfWeek = new ArrayList<>();
-
-        this.initDaysOfWeek();
-
-        final TasksAdapter tasksAdapter  = new TasksAdapter(this, daysOfWeek);
-
         ListView listView = findViewById(R.id.listView);
+        Button dateButton = findViewById(R.id.dateButtonId);
+        Button timeButton = findViewById(R.id.timeButtonId);
+        Button confirmButton = findViewById(R.id.confirmButtonId);
+
+        dateButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                setDaysOfWeek();
+            }
+        });
+
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "Time Picker");
+            }
+        });
 
         listView.setAdapter(tasksAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -42,13 +61,48 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void initDaysOfWeek() {
-        this.daysOfWeek.add(new DayOfWeek(Days.MONDAY, 8));
-        this.daysOfWeek.add(new DayOfWeek(Days.TUESDAY, 8));
-        this.daysOfWeek.add(new DayOfWeek(Days.WEDNESDAY, 8));
-        this.daysOfWeek.add(new DayOfWeek(Days.THURSDAY, 10));
-        this.daysOfWeek.add(new DayOfWeek(Days.FRIDAY, 6));
-        this.daysOfWeek.add(new DayOfWeek(Days.SATURDAY, 5));
-        this.daysOfWeek.add(new DayOfWeek(Days.SUNDAY, 2));
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Toast.makeText(MainActivity.this, hourOfDay + " horas e " + minute + " minutos", Toast.LENGTH_LONG).show();
+        tasksAdapter.notifyDataSetChanged();
+    }
+
+    private void setDaysOfWeek() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Escolha um dia da semana");
+
+         final String[] daysOfWeek = {
+                Days.MONDAY.getValue(),
+                Days.TUESDAY.getValue(),
+                Days.WEDNESDAY.getValue(),
+                Days.THURSDAY.getValue(),
+                Days.FRIDAY.getValue(),
+                Days.SATURDAY.getValue(),
+                Days.SUNDAY.getValue(),
+        };
+
+        int checkedItem = 0;
+
+        builder.setSingleChoiceItems(daysOfWeek, checkedItem, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, daysOfWeek[which], Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setNegativeButton("Cancelar", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void confirmTask() {
+
     }
 }
